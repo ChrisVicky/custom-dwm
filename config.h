d33d991dd6885e4646a5ff9913458bdeb6d8fe13
Christopher <1051666563@qq.com>
2022-11-05 17:38:01 +0800

    Modify SUPER+RETURN to lauch kitty instead of "zoom"

--- config.h
+++ config.h
@@ -75,6 +75,7 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd_kitty[] = { "kitty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -89,7 +90,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	// { MODKEY,                       XK_Return, zoom,           {0} }, // Not so useful
+	{ MODKEY,             		XK_Return, spawn,          {.v = termcmd_kitty } },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
